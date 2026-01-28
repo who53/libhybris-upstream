droidian/libhybris

hybris: egl: wrap eglSetDamageRegionKHR
Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

hybris/egl/egl.c
@@ -451,6 +451,7 @@ static EGLSurface _my_eglCreatePlatformWindowSurface(EGLDisplay dpy, EGLConfig c
 
 HYBRIS_EGL_IMPLEMENT_FUNCTION3(egl, EGLSurface, eglCreatePbufferSurface, EGLDisplay, EGLConfig, const EGLint *);
 HYBRIS_EGL_IMPLEMENT_FUNCTION4(egl, EGLSurface, eglCreatePixmapSurface, EGLDisplay, EGLConfig, EGLNativePixmapType, const EGLint *);
+HYBRIS_EGL_IMPLEMENT_FUNCTION4(egl, EGLBoolean, eglSetDamageRegionKHR, EGLDisplay, EGLSurface, EGLint *, EGLint);
 
 EGLBoolean eglDestroySurface(EGLDisplay dpy, EGLSurface surface)
 {

who53/libhybris-upstream

hybris/egl/egl.c
@@ -379,6 +379,7 @@ static EGLSurface _my_eglCreatePlatformWindowSurfaceEXT(EGLDisplay dpy, EGLConfi
 
 HYBRIS_IMPLEMENT_FUNCTION3(egl, EGLSurface, eglCreatePbufferSurface, EGLDisplay, EGLConfig, const EGLint *);
 HYBRIS_IMPLEMENT_FUNCTION4(egl, EGLSurface, eglCreatePixmapSurface, EGLDisplay, EGLConfig, EGLNativePixmapType, const EGLint *);
+HYBRIS_IMPLEMENT_FUNCTION4(egl, EGLBoolean, eglSetDamageRegionKHR, EGLDisplay, EGLSurface, EGLint *, EGLint);
 
 EGLBoolean eglDestroySurface(EGLDisplay dpy, EGLSurface surface)
 {